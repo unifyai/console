d577068d2681f5d49c115e9fe5574f538b551463
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2024-12-11 14:43:47 +0530

    moved the header further up to avoid scrolled text from appearing between rheader rows

--- src/components/Common/Tables/Data/Base.tsx
+++ src/components/Common/Tables/Data/Base.tsx
@@ -108,7 +108,7 @@ export default function DataTable<TData, TValue>({ data, columns, state, setStat
             sensors={sensors}
         >
             <Table className="sticky top-0 z-10 max-h-[90vh] w-full border-1" style={{ ...columnSizeVars }}>
-                <TableHeader className="sticky -top-[1px] z-10 bg-background">
+                <TableHeader className="sticky -top-[6px] z-10 bg-background">
                     {table.getHeaderGroups().map((headerGroup) => (
                         <TableRow key={headerGroup.id}>
                             <SortableContext items={state.columnOrder} strategy={horizontalListSortingStrategy}>
