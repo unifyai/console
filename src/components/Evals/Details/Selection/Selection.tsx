204e431a9fcf6e4215e1683b0c7651f6a9a930d2
nassimberrada <112006029+nassimberrada@users.noreply.github.com>
2024-12-16 16:23:52 +0000

    Add toggle to group identical selection entries

--- src/components/Evals/Details/Selection/Selection.tsx
+++ src/components/Evals/Details/Selection/Selection.tsx
@@ -6,20 +6,29 @@ import SelectionHints from "./Hints";
 import SelectionEntry from "./Entry";
 import { useQueryState } from "nuqs";
 import SettingButton from "@/components/Common/Buttons/Setting";
-import {Columns2, Columns4} from "lucide-react";
+import {GalleryVerticalEnd, GalleryHorizontalEnd, Group, Ungroup} from "lucide-react";
 const Selection = ({ params, logs }: { params: LogItemProps, logs: LogProps[] | undefined }) => {
     
     // Toggling between inline and pairwise comparison
     const [comparisonMode, setComparisonMode] = useQueryState("comparisonMode");
-    const icon = comparisonMode === "pairwise" ? <Columns4/> : <Columns2/>
-    const tooltip = comparisonMode ? "Inline compare" : "Pairwise compare" 
-    const comparisonToggle =    <div className={"absolute top-0 right-0"}>
-                                    <SettingButton 
-                                        tooltip={tooltip} 
-                                        icon={icon} 
-                                        onClick={() => setComparisonMode(pairwise => pairwise != "multiple" ? "multiple" : "pairwise")}
-                                    />
-                                </div>
+    const modeIcon = comparisonMode === "block" ? <GalleryVerticalEnd/> : <GalleryHorizontalEnd/>
+    const modeTooltip = comparisonMode === "block" ? "Inline compare" : "Block compare"  
+    const comparisonToggle =    <SettingButton 
+                                    tooltip={modeTooltip} 
+                                    icon={modeIcon} 
+                                    onClick={() => setComparisonMode(mode => mode != "inline" ? "inline" : "block")}
+                                />
+
+    // Toggling between grouped and individual comparison
+    const [groupedComparison, setGroupedComparison] = useQueryState("groupedComparison");
+    const groupingIcon = groupedComparison === "individual" ? <Ungroup/> : <Group/>
+    const groupingTooltip = groupedComparison === "grouped" ? "Ungroup identical" : "Group identical" 
+    const groupedToggle =   <SettingButton 
+                                tooltip={groupingTooltip} 
+                                icon={groupingIcon} 
+                                onClick={() => setGroupedComparison(grouped => grouped != "grouped" ? "grouped" : "individual")}
+                            />
+
 
     // Get base and comparison logs
     const [comparisonLogsParam,] = useQueryState("comparison");
@@ -32,7 +41,7 @@ const Selection = ({ params, logs }: { params: LogItemProps, logs: LogProps[] | 
     const paramsNodes =     (baseLog: LogProps) =>
                             <div className="flex flex-col gap-2">
                                 <p className="font-bold text-lg">Parameters</p>
-                                <div className={`flex flex-col gap-1 rounded-md border-2 border-gray-200 min-h-10 ${comparisonMode === "multiple" ? "overflow-y-auto" : ""}`}>
+                                <div className={`flex flex-col gap-1 rounded-md border-2 border-gray-200 min-h-10 ${comparisonMode === "inline" ? "overflow-x-auto" : ""}`}>
                                     {Object.entries(extractParamsValues(baseLog.params, params)).map(([property, value], index) => {
                                         return <SelectionEntry 
                                             key={index} 
@@ -51,7 +60,7 @@ const Selection = ({ params, logs }: { params: LogItemProps, logs: LogProps[] | 
     const entriesNodes =    (baseLog: LogProps) => 
                             <div className="flex flex-col gap-2">
                                 <p className="font-bold text-lg">Entries</p>
-                                <div className={`flex flex-col gap-1 rounded-md border-2 border-gray-200 min-h-10 ${comparisonMode === "multiple" ? "overflow-y-auto" : ""}`}>
+                                <div className={`flex flex-col gap-1 rounded-md border-2 border-gray-200 min-h-10 ${comparisonMode === "inline" ? "overflow-x-auto" : ""}`}>
                                     {Object.entries(baseLog.entries).map(([property, value], index) => {
                                         return <SelectionEntry
                                             key={index} 
@@ -71,9 +80,16 @@ const Selection = ({ params, logs }: { params: LogItemProps, logs: LogProps[] | 
         <div className="bg-background rounded-md w-full h-full overflow-y-scroll p-5 flex flex-col">
             {baseLog
             ? <div className="relative gap-4 flex flex-col">
-                {comparisonLogs.length > 1 && comparisonToggle}
+                {comparisonLogs.length > 1 && 
+                    <div className="flex flex-row gap-2 absolute top-0 right-0">
+                        {groupedToggle}
+                        {comparisonToggle}
+                    </div>
+                }
+                <div className={`${comparisonMode === "inline" ? "overflow-x-scroll" : ""}`}>
                 {Object.keys(baseLog.params).length > 0 && paramsNodes(baseLog)}
                 {entriesNodes(baseLog)}
+                </div>
               </div>
             :
             <div className="flex items-center justify-center h-full w-full">
