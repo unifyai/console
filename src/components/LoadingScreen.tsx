e8ada55e2e819d6cad78bfecd4c35aa7642d1800
jkeane508 <iamjameskeane@users.noreply.github.com>
2024-12-17 14:01:42 +0000

    feat: update loading screen to use a better animation

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -4,7 +4,7 @@ import { Loader2 } from 'lucide-react';
 const LoadingScreen: React.FC = () => {
     return (
         <div className="fixed inset-0 flex flex-col items-center justify-center bg-background z-50">
-            <Loader2 className="animate-spin h-20 w-20 text-accent mb-10" />
+            <Loader2 className="animate-spin py-5" />
             <p className="text-foreground text-lg font-semibold">Loading...</p>
         </div>  
     );
