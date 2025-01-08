4d5117ea1614070ec2edb4f7900e2ee071770ca9
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-01-08 18:02:52 +0530

    fixed a few more build failures

--- src/components/Evals1/Details/Details.tsx
+++ src/components/Evals1/Details/Details.tsx
@@ -12,32 +12,33 @@ const Details = ({ params, logs, fullLogs}: {
     fullLogs: LogProps[]
 }) => {
     return (
-        <Tabs defaultValue="View" className="w-full h-full tutorial-details-panel">
-            <TabsList className="bg-background rounded-md w-full justify-between py-8 px-4">
-                <div className="flex flex-row gap-3">
-                    <TabsTrigger 
-                    value="View"
-                    className="flex flex-row gap-2 data-[state=active]:text-accent hover:text-primary"
-                    >
-                        <Eye/>
-                        {"View"}
-                    </TabsTrigger>
-                    <TabsTrigger 
-                    value="Plot" 
-                    className="flex flex-row gap-2 data-[state=active]:text-accent hover:text-primary"
-                    >
-                        <ScatterChart/>
-                        {"Plot"}
-                    </TabsTrigger>
-                </div>
-            </TabsList>
-            <TabsContent value="View" className="w-full h-[calc(100%-50px)] tutorial-selection-pane">
-                <Selection params={params} logs={logs} />
-            </TabsContent>
-            <TabsContent value="Plot" className="w-full h-[calc(100%-50px)] tutorial-plot-pane">
-                <LogsPlot logs={fullLogs} />
-            </TabsContent>
-        </Tabs>
+        <div>text</div>
+        // <Tabs defaultValue="View" className="w-full h-full tutorial-details-panel">
+        //     <TabsList className="bg-background rounded-md w-full justify-between py-8 px-4">
+        //         <div className="flex flex-row gap-3">
+        //             <TabsTrigger 
+        //             value="View"
+        //             className="flex flex-row gap-2 data-[state=active]:text-accent hover:text-primary"
+        //             >
+        //                 <Eye/>
+        //                 {"View"}
+        //             </TabsTrigger>
+        //             <TabsTrigger 
+        //             value="Plot" 
+        //             className="flex flex-row gap-2 data-[state=active]:text-accent hover:text-primary"
+        //             >
+        //                 <ScatterChart/>
+        //                 {"Plot"}
+        //             </TabsTrigger>
+        //         </div>
+        //     </TabsList>
+        //     <TabsContent value="View" className="w-full h-[calc(100%-50px)] tutorial-selection-pane">
+        //         <Selection params={params} logs={logs} />
+        //     </TabsContent>
+        //     <TabsContent value="Plot" className="w-full h-[calc(100%-50px)] tutorial-plot-pane">
+        //         <LogsPlot logs={fullLogs} />
+        //     </TabsContent>
+        // </Tabs>
     );
 };
 
