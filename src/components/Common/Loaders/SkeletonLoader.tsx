e857fdc1eb6e9952c065bf243042f24e4bc32605
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-01-13 14:51:23 +0530

    the bg color for the skeleton loader wasn't valid so it wasn't visible, added muted for now

--- src/components/Common/Loaders/SkeletonLoader.tsx
+++ src/components/Common/Loaders/SkeletonLoader.tsx
@@ -6,7 +6,7 @@ import { Skeleton } from "@/components/UI/skeleton";
 const SkeletonLoader = () => {
     return (
         <Skeleton className="flex rounded-md h-full w-full">
-            <div className="w-full rounded-md bg-default-300"/>
+            <div className="w-full rounded-md bg-muted"/>
         </Skeleton>
     );
 };
