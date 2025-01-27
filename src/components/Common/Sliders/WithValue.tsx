43bf09531daeb9cc61a27a42e94d4cd08f5adeca
nassimberrada <112006029+nassimberrada@users.noreply.github.com>
2025-01-27 15:07:01 +0000

    Handle out of bound values for bin count input

--- src/components/Common/Sliders/WithValue.tsx
+++ src/components/Common/Sliders/WithValue.tsx
@@ -14,15 +14,25 @@ const SliderWithValue = ({label, ticks, value, setValue, max, min}: {
   const maximum = max ? max : Math.max(...ticks);
   const minimum = min ? min : Math.min(...ticks);
 
+  const onChange = (e: any) => {
+    const value = +e.currentTarget.value as number
+    if (value < minimum)
+      setValue(minimum)
+    else if (value > maximum)
+      setValue(maximum)
+    else setValue(value)
+  }
   return (
     <div className="flex flex-col gap-2 p-2 pb-3">
         <div className="flex items-center gap-5 justify-between">            
             <Label>{label}</Label>
             <Input 
+              min={minimum}
+              max={maximum}
               type="number" 
               value={value} 
-              onChange={(e) => setValue(+e.currentTarget.value)}
-              className="h-8 max-w-[50px]"
+              onChange={onChange}
+              className="h-8 max-w-[80px]"
             />
         </div>
         <Slider value={[value]} max={maximum} min={minimum} aria-label="Slider with ticks" onValueChange={(v) => setValue(v[0])} />
