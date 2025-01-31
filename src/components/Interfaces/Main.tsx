db60b5172c46720d05d8d32bc9a6144066ba9c07
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-01-31 17:07:08 +0530

    got the interface switch bug fixed and added cookies for retaining last state

--- src/components/Interfaces/Main.tsx
+++ src/components/Interfaces/Main.tsx
@@ -4,6 +4,8 @@ import { getLogsDetails } from "@/utils/evals/common";
 import { FieldsActions, Interface, InterfaceActions, LogsActions, PlotDataProps, ProjectsActions, TableDataProps } from "@/types/evals/grid";
 import { searchParamToFilters, filtersToExpression } from "@/utils/evals/filters";
 import { processContext } from "@/utils/evals/columnOperations";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 const Main = async ({ interface_, project_, projectsActions, logsActions, fieldsActions, interfaceActions }: {
     interface_: string | undefined,
@@ -13,9 +15,13 @@ const Main = async ({ interface_, project_, projectsActions, logsActions, fields
     fieldsActions: FieldsActions,
     interfaceActions: InterfaceActions
 }) => {
+    const cookies_ = cookies();
+    const cookiesProject = cookies_.get("project")?.value;
+    const cookiesInterface = cookies_.get("interface")?.value;
+
     // Get projects
     const projects: string[] = await projectsActions.get();
-    const project = projects.find(proj => proj == project_) || null;
+    const project = projects.find(proj => proj == (project_ || cookiesProject)) || null;
 
     // Get interface
     let interfaces_: { [key: string]: Interface } = (
@@ -25,7 +31,9 @@ const Main = async ({ interface_, project_, projectsActions, logsActions, fields
         (project ? await interfaceActions.get(project, true) : []) || []
     ).reduce((acc, curr) => ({...acc, [curr.name]: curr}), {});
     let interfaceCreated = interface_ != undefined && interface_ in interfaces_;
-    const interface_1 = Object.keys(interfacesTemp_).find(i => i == interface_) || (
+    const interface_1 = Object.keys(interfacesTemp_).find(i => i == (interface_ || (
+        project == cookiesProject ? cookiesInterface : undefined
+    ))) || (
         Object.keys(interfacesTemp_).length ? Object.keys(interfacesTemp_).sort()[0] : null
     );
     let currentInterface = (interface_1 && interface_1 in interfacesTemp_) ? interfacesTemp_[interface_1] : null;
@@ -35,6 +43,8 @@ const Main = async ({ interface_, project_, projectsActions, logsActions, fields
         items: [],
         new_counter: 0
     } as Interface;
+    if (!interface_)
+        redirect(`/interfaces?project=${project}&interface=${interface_1}`);
 
     // Get fields
     let fields: LogFieldsResponseProps = {};
@@ -210,6 +220,7 @@ const Main = async ({ interface_, project_, projectsActions, logsActions, fields
     )).reduce((acc, curr) => ({ ...acc, ...curr }), {});
 
     return <CardGrid
+        project_={project}
         projects={projects}
         interfaces_={Object.keys(interfacesTemp_).sort()}
         tableNames={tableNames}
