f0d78373cf8193a980ad67bd5a4410f6cf04de2a
jkeane508 <iamjameskeane@users.noreply.github.com>
2025-02-04 04:56:55 +0000

    Chore: Port Evals View Pane Updates to Interfaces

--- src/components/Interfaces/Details/Selection/Views/NumberView.tsx
+++ src/components/Interfaces/Details/Selection/Views/NumberView.tsx
@@ -134,8 +134,10 @@ export default function NumberView({
           <div className="space-y-2">
             <p className="font-semibold">Version</p>
             {baseVer ? (
-              <div className="p-2 relative">
-                <MarkdownRenderer>{baseVer}</MarkdownRenderer>
+              <div className="flex p-2 relative">
+                <div>
+                  <MarkdownRenderer>{baseVer}</MarkdownRenderer>
+                </div>
                 <CopyButton
                   className="absolute top-1 right-1 text-gray-400 hover:text-gray-700"
                   content={baseVer}
@@ -151,8 +153,10 @@ export default function NumberView({
 
         <div className="space-y-2">
           {!versionEmpty && <p className="font-semibold">Value</p>}
-          <div className="border rounded p-2 relative">
-            <p className="text-sm p-2">{baseNum}</p>
+          <div className="flex border rounded p-2 relative">
+            <div>
+              <p className="text-sm">{baseNum}</p>
+            </div>
             <CopyButton
               className="absolute top-1 right-1 text-gray-400 hover:text-gray-700"
               content={String(baseNum)}
@@ -191,11 +195,11 @@ export default function NumberView({
             <div key={i} className="p-3 space-y-4">
               {!versionEmpty && (
                 <div className="space-y-2">
-                  <p className="font-semibold">Param Version</p>
+                  <p className="font-semibold">Version</p>
                   {verGroups.map((vg, j) => (
                     <div
                       key={j}
-                      className="border rounded p-2 relative"
+                      className="flex border rounded p-2 relative"
                     >
                       <RowBadge rowNumbers={vg.rows} mode="none" />
                       {vg.text ? (
@@ -212,14 +216,17 @@ export default function NumberView({
                 </div>
               )}
 
-              <div className="border rounded p-2 bg-background relative">
-                <RowBadge rowNumbers={rowNums} mode="none" />
-                <CopyButton
-                  className="absolute top-1 right-1 text-gray-400 hover:text-gray-700"
-                  content={String(numVal)}
-                  copyMessage="Copied number!"
-                />
-                <p className="text-sm font-semibold mt-4">{numVal}</p>
+              <div className="space-y-2">
+                {!versionEmpty && <p className="font-semibold">Value</p>}
+                <div className="border rounded p-2 bg-background relative">
+                  <RowBadge rowNumbers={rowNums} mode="none" />
+                  <CopyButton
+                    className="absolute top-1 right-1 text-gray-400 hover:text-gray-700"
+                    content={String(numVal)}
+                    copyMessage="Copied number!"
+                  />
+                  <p className="text-sm mt-2">{numVal}</p>
+                </div>
               </div>
             </div>
           );
@@ -308,7 +315,7 @@ export default function NumberView({
                 <div className="relative border rounded p-2 w-fit min-w-24 text-start">
                   <div className="flex-col items-start justify-between gap-5">
                     <RowBadge rowNumbers={[baseLogIndex]} mode="none" />
-                    <p className="text-sm font-semibold pt-5">{baseNum}</p>
+                    <p className="text-sm pt-5">{baseNum}</p>
                   </div>
                   <CopyButton
                     className="absolute top-1 right-1 text-gray-400 hover:text-gray-700"
@@ -324,7 +331,7 @@ export default function NumberView({
                 <div className="relative border rounded p-2 w-fit min-w-24 text-start">
                   <div className="flex-col items-start justify-between gap-5">
                     <RowBadge rowNumbers={rowNums} mode="none" />
-                    <p className="text-sm font-semibold pt-5">{compVal}</p>
+                    <p className="text-sm pt-5">{compVal}</p>
                   </div>
                   <CopyButton
                     className="absolute top-1 right-1 text-gray-400 hover:text-gray-700"
@@ -340,9 +347,9 @@ export default function NumberView({
                   <div className="flex-col items-start justify-between gap-5">
                     <p>Result</p>
                     {Number.isFinite(result) ? (
-                      <p className="text-sm font-semibold pt-5">{result}</p>
+                      <p className="text-sm pt-5">{result}</p>
                     ) : (
-                      <p className="text-sm font-semibold pt-5">∞</p>
+                      <p className="text-sm pt-5">∞</p>
                     )}
                   </div>
                   <CopyButton
