0bb7bb109e61063db13a8a33a83819a9c10699c6
nassimberrada <112006029+nassimberrada@users.noreply.github.com>
2025-02-10 18:26:13 +0000

    Add copy button to cell popover

--- src/components/Interfaces/Table/Content/CellPopover.tsx
+++ src/components/Interfaces/Table/Content/CellPopover.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { BasePopover } from "@/components/Common/Popovers/Base";
+import { CopyButton } from "@/components/Common/Buttons/Copy";
 import { Cell, flexRender } from "@tanstack/react-table";
 import { Dispatch, SetStateAction, useState, useEffect } from "react";
 
@@ -28,9 +29,14 @@ const CellPopover = ({cell, isCellExpanded, setExpandedCells}: {
         else setOpen(false)
     }, [isCellExpanded(cell)])
 
+    /* Copy button */
+    const content = JSON.stringify(cell.renderValue())
+    const copy = <div className="absolute top-1 right-1"><CopyButton content={content}/></div>
+    
     return (
-    <div style={{position: "absolute"}}>
-        <BasePopover button={null} open={open} setOpen={setOpen} className="max-w-[500px] max-h-[200px] overflow-auto p-5">
+    <div style={{position: "absolute"}} onClick={(e) => e.stopPropagation()}>
+        <BasePopover button={null} open={open} setOpen={setOpen} className="relative max-w-[500px] max-h-[200px] overflow-auto p-5">
+            {copy}
             {flexRender(cell.column.columnDef.cell, cell.getContext())}
         </BasePopover>
     </div>
