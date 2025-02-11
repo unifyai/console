56bd96f8165932998991212a0d39b0da10d24875
nassimberrada <112006029+nassimberrada@users.noreply.github.com>
2025-02-11 16:07:23 +0000

    Add loader to delete button

--- src/components/Common/Buttons/Delete.tsx
+++ src/components/Common/Buttons/Delete.tsx
@@ -1,5 +1,13 @@
 import BaseButton from "./Base";
+import { LoaderCircle } from "lucide-react";
 
-export default function DeleteButton ({disabled = false, onClick}: {disabled?: boolean, onClick?: () => void}) {
-    return <BaseButton text="Delete" disabled={disabled} variant="destructive" type="button" onClick={onClick}/>
+export default function DeleteButton ({disabled = false, onClick, loading}: {loading?: boolean, disabled?: boolean, onClick?: () => void}) {
+    return <BaseButton 
+        icon={loading ? <LoaderCircle className="animate-spin text-white"/> : undefined} 
+        text="Delete" 
+        disabled={disabled || loading} 
+        variant="destructive" 
+        type="button" 
+        onClick={onClick}
+    />
 }
