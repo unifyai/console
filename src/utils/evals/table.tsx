f598f51ad8654a38d5c10d5a3c42afeb3da6a4dc
Haris Mahmood <hmahmood24@users.noreply.github.com>
2025-02-12 14:49:15 +0500

    Fix adding groupingIndex to Row instance

--- src/utils/evals/table.tsx
+++ src/utils/evals/table.tsx
@@ -769,7 +769,7 @@ export function getCoreRowModel<TData extends RowData>(): (
 			// Add necessary props to support manual server-side grouping
             row.groupingColumnId = (originalRows[i] as any).groupingColumnId
             row.groupingValue = (originalRows[i] as any)[row.groupingColumnId]
-			row.groupingIndex = (originalRows[i] as any)[row.groupingIndex]
+			row.groupingIndex = (originalRows[i] as any).groupingIndex
 
             // Keep track of every row in a flat array
             rowModel.flatRows.push(row)
