52b3bc12d298091089b06bb08bd5e5c1174753b7
Haris Mahmood <hmahmood24@users.noreply.github.com>
2025-02-14 13:46:37 +0500

    Added vercel deployed subdomain to allowedOrigins for executing server actions. Can be removed either if stay on GCP or migrate the domains over to Vercel altogether

--- next.config.js
+++ next.config.js
@@ -30,7 +30,7 @@ const nextConfig = {
         instrumentationHook: true,
         serverMinification: false,
         serverActions: {
-            allowedOrigins: ["unify.ai"]
+            allowedOrigins: ["unify.ai", "console-unifyai.vercel.app"]  // TODO: Remove this once Vercel is pointing to `unify.ai` domain
         }
     },
     compiler: {
