c90f07ff307af06b067585dc7ebbea097925fbae
nassimberrada <112006029+nassimberrada@users.noreply.github.com>
2025-02-18 17:40:18 +0000

    Enable backspacing full value in plot bins value input

--- src/components/Interfaces/Details/Plot/Buttons/PlotBins.tsx
+++ src/components/Interfaces/Details/Plot/Buttons/PlotBins.tsx
@@ -11,9 +11,19 @@ const PlotBins = ({binCount, setBinCount, binCounts}: {
     setBinCount: (newValue: string | undefined) => void
 }) => {
     const button = <SettingButton icon={<ChartColumnBig/>} tooltip={"Bin count"} variant={"outline"}/>
+    const setValue = (value: any) => {
+        let update;
+        if (typeof value === "number") {
+            if (value < binCounts[0]) update = binCounts[0]
+            else if (value > binCounts[1]) update = binCounts[1]
+            else update = value
+        } 
+        else update = binCounts[0]
+        setBinCount(update.toString())
+    }
     return (
         <BaseDropdown button={button} side="left">
-            <SliderWithValue label="Bin count" value={binCount} setValue={(value) => setBinCount(value.toString())} ticks={binCounts}/>
+            <SliderWithValue label="Bin count" value={binCount} setValue={setValue} ticks={binCounts}/>
         </BaseDropdown>
     );
 }
