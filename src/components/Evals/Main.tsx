656bb7c35bb2aedee71c59cf53848f55ee94dce8
Daniel Lenton <dan@unify.ai>
2025-02-21 10:36:27 +0700

    reduced page limit to 20

--- src/components/Evals/Main.tsx
+++ src/components/Evals/Main.tsx
@@ -76,7 +76,7 @@ const Main = async ({ searchParams, projectsActions, logsActions, fieldsActions,
 	/* Get logs with pagination, and plot logs subset */
 	
 	let logsData: LogsResponseProps = { params: {}, logs: [], count: 0, groups: {} };
-	const limit = 100;
+	const limit = 20;
 	const offset = (searchParams.page_number ? parseInt(searchParams.page_number) : 0) * limit;
 	let totalPages = 1;
 	let plotData: LogsResponseProps = { params: {}, logs: [], count: 0, groups: {} };
