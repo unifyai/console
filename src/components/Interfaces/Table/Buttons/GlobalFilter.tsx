f684285ab8f2c94b0b41cba0a047773a913a9452
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-02-26 10:36:21 +0530

    updated the global filter code to avoid breaking when the table has been added but the server reload isn't complete

--- src/components/Interfaces/Table/Buttons/GlobalFilter.tsx
+++ src/components/Interfaces/Table/Buttons/GlobalFilter.tsx
@@ -53,8 +53,8 @@ const GlobalFilter = ({ interactive, commonFilter, setCommonFilter, logs, setLog
     /* Expression input */
     const options = Object
         .entries(tableArguments)
-            .map(([table, args]) => ({name: table, type: "Table Name", children: Object.keys(args.available_fields)}))  // Add all displayed tables
-        .concat(Object.entries(tableArguments[currentTable as keyof TableArguments].available_fields)                   // Add all columns of current table
+            .map(([table, args]) => ({name: table, type: "Table Name", children: Object.keys(args?.available_fields ?? {})}))  // Add all displayed tables
+        .concat(Object.entries(tableArguments[currentTable as keyof TableArguments]?.available_fields ?? {})                   // Add all columns of current table
             .map(([column, _]) => ({name: column, type: "Column Name", children: []}))
         )
     const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
