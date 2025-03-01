147493cf316ea9b2b70c93a8457c1818557ac9e4
djl11 <dan@unify.ai>
2025-03-01 19:25:15 +0700

    Revert "Add spinner loader to page controllers and update current page tooltip"
    This reverts commit 8c687f4841035db56244857f83d674d982d54ac0.

--- src/components/Interfaces/Table/Table.tsx
+++ src/components/Interfaces/Table/Table.tsx
@@ -404,8 +404,6 @@ const LogsTable = ({
             setPageNumber={updateItem(item, "page_number")}
             pageLogs={logs.length}
             totalLogs={logsData.count}
-            logs={logs}
-            limit={limit}
           />
           <FreezeLogs item={item} updateItem={updateItem} />
           <RefreshLogs
