6708881a2b075918b2eb4f8c7a969d5397b7643f
Haris Mahmood <hmahmood24@users.noreply.github.com>
2025-03-05 21:06:51 +0500

    Implemented the state context provider and updater for the zustand store

---
+++ src/contexts/providers/StoreUpdater.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React, { useEffect, useRef } from 'react';
+import { IStoreState } from '../store';
+import { useStoreContext } from './StoreProvider';
+
+// Component to handle store updates when initialState changes
+function StoreUpdater({ initialState }: { initialState: Partial<IStoreState> }) {
+  const resetState = useStoreContext(state => state.resetState);
+  const initialStateRef = useRef(initialState);
+  
+  useEffect(() => {
+    // Check if initialState has changed from the previous value
+    if (initialStateRef.current !== initialState) {
+      // Update the store with the new state
+      resetState(initialState);
+      // Update the ref to the current initialState
+      initialStateRef.current = initialState;
+    }
+  }, [initialState, resetState]);
+  
+  return null; // This component doesn't render anything
+}
+
+export default StoreUpdater;
