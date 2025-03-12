f24f01f022a562953aeaf50c5d69c0f416d484d6
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-03-12 20:55:12 +0530

    got the maximize view for the gif added

--- src/components/Interfaces/DefaultProject.tsx
+++ src/components/Interfaces/DefaultProject.tsx
@@ -11,6 +11,8 @@ import { useQueryState } from "nuqs";
 import BaseDropdown from "../Common/Dropdowns/Base";
 import { DropdownMenuItem } from "../UI/dropdown-menu";
 import Link from "next/link";
+import { DialogContent } from "../UI/dialog";
+import { Dialog } from "../UI/dialog";
 
 const DefaultProject = ({
     projects,
@@ -31,6 +33,7 @@ const DefaultProject = ({
 }) => {
     const disabled = projects == undefined
     const [pendingLocal, setPendingLocal] = useState(false);
+    const [imageDialog, setImageDialog] = useState(false);
     const [example, setExample] = useQueryState("example");
     const [create, setCreate] = useQueryState("create");
     const reorganizedExamples: {
@@ -151,106 +154,53 @@ const DefaultProject = ({
         }
     });
 
-    return <div className="flex flex-col gap-4 justify-center items-center">
-        <div className="mt-4 flex justify-center font-semibold">
-            Please select a project, create a project or get started some of the examples below
-        </div>
-        <BaseDropdown
-            button={<ActionButton
-                tooltip={"Select Example"}
-                text={example ? example.split("/")[1] : "Select Example"}
-                variant={"outline"}
-                size="default"
-            />}
-        >
-            <div className="max-h-[80vh] overflow-y-auto">
-                {Object.keys(reorganizedExamples).map((group) => {
-                    return <div key={group} className="w-[300px] mt-2 pb-1 px-3 border-b">
-                        <div className="font-semibold text-sm mb-1">{group}</div>
-                    {Object.keys(reorganizedExamples[group]).map(ex =>
-                        <DropdownMenuItem key={ex} onClick={() => setExample(`${group}/${ex}`)}>
-                            {ex}
-                        </DropdownMenuItem>
-                    )}
-                    </div>
-                })}
+    return <>
+        <div className="flex flex-col gap-4 justify-center items-center">
+            <div className="mt-4 flex justify-center font-semibold">
+                Please select a project, create a project or get started some of the examples below
             </div>
-        </BaseDropdown>
-        <div className="relative max-w-[700px] max-h-[700px] overflow-y-auto rounded-md border border-1 p-2">
-            <div className="absolute z-10 top-3 right-12 flex gap-1">
-                <Link href={`https://docs.unify.ai/${exampleLink}`} target="_blank">
-                    <ActionButton icon={<ExternalLink />} tooltip={"Learn more"} />
-                </Link>
-                <ActionButton
-                    icon={pendingLocal ? <Loader2 className="animate-spin" /> : <Play />}
-                    tooltip={"Run Example"}
-                    onClick={() => {
-                        if (projects?.includes(exampleProject)) {
-                            setProject(exampleProject);
-                        } else {
-                            setPendingLocal(true);
-                            projectActions.create(exampleProject).then(() => {
-                                interfaceActions.create(
-                                    exampleName, exampleProject, undefined, exampleItems, exampleNewCounter, true
-                                ).then(() => {
-                                    if (exampleProject == "context-demo") {
-                                        Promise.all(Object.keys(exampleLogs).map(context => logsActions.create(
-                                            exampleProject, context, exampleLogs[context].params, exampleLogs[context].entries
-                                        ))).then(() => {
-                                            setTimeout(() => {
-                                                setPendingLocal(false);
-                                                setProject(exampleProject);
-                                                setInterface(exampleName);
-                                                setExample(null);
-                                                setCreate(null);
-                                            }, 3000);
-                                        });
-                                    }
-                                    else if (exampleProject == "MarkingAssistant") {
-                                        const context = Object.keys(exampleLogs)[0];
-                                        const length = exampleLogs[context].entries.length;
-                                        Promise.all(
-                                            Array.from(
-                                                { length: Math.ceil(length / 100) },
-                                                (_, i) => ({ i: i * 100, j: Math.min((i + 1) * 100, length) })
-                                            ).map(({ i, j }) => logsActions.create(
-                                                exampleProject,
-                                                context,
-                                                exampleLogs[context].params,
-                                                exampleLogs[context].entries.slice(i, j)
-                                            ))
-                                        ).then(() => {
-                                            setTimeout(() => {
-                                                setPendingLocal(false);
-                                                setProject(exampleProject);
-                                                setInterface(exampleName);
-                                                setExample(null);
-                                                setCreate(null);
-                                            }, 3000);
-                                        });
-                                    }
-                                    else {
-                                        logsActions.create(
-                                            exampleProject, null, exampleLogs.params, exampleLogs.entries
-                                        ).then(() => {
-                                            if (exampleDerivedColumns != undefined) {
-                                                derivedEntryActions.create(
-                                                    exampleDerivedColumns.project,
-                                                    exampleDerivedColumns.context,
-                                                    exampleDerivedColumns.key,
-                                                    exampleDerivedColumns.equation,
-                                                    exampleDerivedColumns.referenced_logs
-                                                ).then(() => {
-                                                    setTimeout(() => {
-                                                        setPendingLocal(false);
-                                                        setProject(exampleProject);
-                                                        setInterface(exampleName);
-                                                        setExample(null);
-                                                        setCreate(null);
-                                                    }, 3000);
-                                                });
-                                            }
-                                            else {
+            <BaseDropdown
+                button={<ActionButton
+                    tooltip={"Select Example"}
+                    text={example ? example.split("/")[1] : "Select Example"}
+                    variant={"outline"}
+                    size="default"
+                />}
+            >
+                <div className="max-h-[80vh] overflow-y-auto">
+                    {Object.keys(reorganizedExamples).map((group) => {
+                        return <div key={group} className="w-[300px] mt-2 pb-1 px-3 border-b">
+                            <div className="font-semibold text-sm mb-1">{group}</div>
+                        {Object.keys(reorganizedExamples[group]).map(ex =>
+                            <DropdownMenuItem key={ex} onClick={() => setExample(`${group}/${ex}`)}>
+                                {ex}
+                            </DropdownMenuItem>
+                        )}
+                        </div>
+                    })}
+                </div>
+            </BaseDropdown>
+            <div className="relative max-w-[700px] max-h-[700px] overflow-y-auto rounded-md border border-1 p-2">
+                <div className="absolute z-10 top-3 right-12 flex gap-1">
+                    <Link href={`https://docs.unify.ai/${exampleLink}`} target="_blank">
+                        <ActionButton icon={<ExternalLink />} tooltip={"Learn more"} />
+                    </Link>
+                    <ActionButton
+                        icon={pendingLocal ? <Loader2 className="animate-spin" /> : <Play />}
+                        tooltip={"Run Example"}
+                        onClick={() => {
+                            if (projects?.includes(exampleProject)) {
+                                setProject(exampleProject);
+                            } else {
+                                setPendingLocal(true);
+                                projectActions.create(exampleProject).then(() => {
+                                    interfaceActions.create(
+                                        exampleName, exampleProject, undefined, exampleItems, exampleNewCounter, true
+                                    ).then(() => {
+                                        if (exampleProject == "context-demo") {
+                                            Promise.all(Object.keys(exampleLogs).map(context => logsActions.create(
+                                                exampleProject, context, exampleLogs[context].params, exampleLogs[context].entries
+                                            ))).then(() => {
                                                 setTimeout(() => {
                                                     setPendingLocal(false);
                                                     setProject(exampleProject);
@@ -258,29 +208,97 @@ const DefaultProject = ({
                                                     setExample(null);
                                                     setCreate(null);
                                                 }, 3000);
-                                            }
-                                        });
-                                    }
+                                            });
+                                        }
+                                        else if (exampleProject == "MarkingAssistant") {
+                                            const context = Object.keys(exampleLogs)[0];
+                                            const length = exampleLogs[context].entries.length;
+                                            Promise.all(
+                                                Array.from(
+                                                    { length: Math.ceil(length / 100) },
+                                                    (_, i) => ({ i: i * 100, j: Math.min((i + 1) * 100, length) })
+                                                ).map(({ i, j }) => logsActions.create(
+                                                    exampleProject,
+                                                    context,
+                                                    exampleLogs[context].params,
+                                                    exampleLogs[context].entries.slice(i, j)
+                                                ))
+                                            ).then(() => {
+                                                setTimeout(() => {
+                                                    setPendingLocal(false);
+                                                    setProject(exampleProject);
+                                                    setInterface(exampleName);
+                                                    setExample(null);
+                                                    setCreate(null);
+                                                }, 3000);
+                                            });
+                                        }
+                                        else {
+                                            logsActions.create(
+                                                exampleProject, null, exampleLogs.params, exampleLogs.entries
+                                            ).then(() => {
+                                                if (exampleDerivedColumns != undefined) {
+                                                    derivedEntryActions.create(
+                                                        exampleDerivedColumns.project,
+                                                        exampleDerivedColumns.context,
+                                                        exampleDerivedColumns.key,
+                                                        exampleDerivedColumns.equation,
+                                                        exampleDerivedColumns.referenced_logs
+                                                    ).then(() => {
+                                                        setTimeout(() => {
+                                                            setPendingLocal(false);
+                                                            setProject(exampleProject);
+                                                            setInterface(exampleName);
+                                                            setExample(null);
+                                                            setCreate(null);
+                                                        }, 3000);
+                                                    });
+                                                }
+                                                else {
+                                                    setTimeout(() => {
+                                                        setPendingLocal(false);
+                                                        setProject(exampleProject);
+                                                        setInterface(exampleName);
+                                                        setExample(null);
+                                                        setCreate(null);
+                                                    }, 3000);
+                                                }
+                                            });
+                                        }
+                                    });
                                 });
-                            });
-                        }
-                    }}
-                    disabled={disabled}
-                />
+                            }
+                        }}
+                        disabled={disabled}
+                    />
+                </div>
+                <MarkdownRender content={`\`\`\`python${exampleCode}\`\`\``} noBackground />
             </div>
-            <MarkdownRender content={`\`\`\`python${exampleCode}\`\`\``} noBackground />
+            {exampleGif != undefined && <div className="mb-1">
+                <Image
+                    key={exampleGif}
+                    src={`https://raw.githubusercontent.com/unifyai/unifyai.github.io/main/img/externally_linked/docs/${exampleGif}.gif`}
+                    alt="GIF"
+                    width={600}
+                    height={600}
+                    unoptimized
+                    onClick={() => setImageDialog(true)}
+                />
+            </div>}
         </div>
-        {exampleGif != undefined && <div className="mb-1">
-            <Image
-                key={exampleGif}
-                src={`https://raw.githubusercontent.com/unifyai/unifyai.github.io/main/img/externally_linked/docs/${exampleGif}.gif`}
-                alt="GIF"
-                width={600}
-                height={600}
-                unoptimized
-            />
-        </div>}
-    </div>
+        <Dialog open={imageDialog} onOpenChange={setImageDialog}>
+            <DialogContent className="max-w-[1400px] max-h-[800px]">
+                <Image
+                    key={exampleGif}
+                    src={`https://raw.githubusercontent.com/unifyai/unifyai.github.io/main/img/externally_linked/docs/${exampleGif}.gif`}
+                    alt="GIF"
+                    width={1400}
+                    height={800}
+                    unoptimized
+                />
+            </DialogContent>
+        </Dialog>
+    </>
 }
 
 export default DefaultProject;
