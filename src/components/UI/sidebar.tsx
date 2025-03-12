3bc227bdb3c31dd13c32375660d420d6e5bdea64
nassimberrada <112006029+nassimberrada@users.noreply.github.com>
2025-03-12 15:45:01 +0000

    Update navmenu toggle icon and add signout and theme buttons to the collapsed menu

--- src/components/UI/sidebar.tsx
+++ src/components/UI/sidebar.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { VariantProps, cva } from "class-variance-authority"
-import { ChevronRight, ChevronLeft } from "lucide-react"
+import { PanelLeft } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
@@ -278,7 +278,7 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      {state === "expanded" ? <ChevronLeft /> : <ChevronRight />}
+      <PanelLeft />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
