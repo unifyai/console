356cbfa7d832ff9ef1e57786634fc0fa15594400
nassimberrada <112006029+nassimberrada@users.noreply.github.com>
2025-03-12 15:48:04 +0000

    Update reset all grouping button icon

--- src/components/Interfaces/Table/Table.tsx
+++ src/components/Interfaces/Table/Table.tsx
@@ -475,9 +475,9 @@ const LogsTable = ({
             setColumnVisibility={setColumnVisibility}
             context={item.context ?? null}
           />
-          <ResetServerAction condition={grouping.length > 0} type={"grouping"} interactive={interactive} logs={logs} setterFunction={() => setGrouping([])}  icon={<Ungroup/>}/>
-          <ResetServerAction condition={(sorting.length > 0 || groupSorting.length > 0)} type={"sorting"} interactive={interactive} logs={logs} setterFunction={() => {setSorting([]); setGroupSorting([])}}  icon={<ListX/>}/>
-          <ResetServerAction condition={(logsFilters != undefined || commonFilter != undefined)} type={"filters"} interactive={interactive} logs={logs} setterFunction={() => {setLogsFilters({}); updateItem(item, "common_filter")(undefined)}}  icon={<FilterX/>}/>
+          <ResetServerAction condition={grouping.length > 0} type={"grouping"} interactive={interactive} logs={logs} setterFunction={() => setGrouping([])}/>
+          <ResetServerAction condition={(sorting.length > 0 || groupSorting.length > 0)} type={"sorting"} interactive={interactive} logs={logs} setterFunction={() => {setSorting([]); setGroupSorting([])}}/>
+          <ResetServerAction condition={(logsFilters != undefined || commonFilter != undefined)} type={"filters"} interactive={interactive} logs={logs} setterFunction={() => {setLogsFilters({}); updateItem(item, "common_filter")(undefined)}}/>
         </div>
       )}
       {project && (
