e49e2d3af9bd4afc0d28078b2b9412ece73622ea
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-03-13 12:10:47 +0530

    updated cursor in the gifs to allow max and min on click, updated the text to it's bottom, vertically centered the contexts, swapped horizontal position

--- src/components/Interfaces/DefaultProject.tsx
+++ src/components/Interfaces/DefaultProject.tsx
@@ -1,19 +1,20 @@
 "use client";
 
 import Image from "next/image";
-import { ExternalLink, Loader2, Play } from "lucide-react";
+import { Check, ExternalLink, Loader2, Play } from "lucide-react";
 import ActionButton from "../Common/Buttons/Action";
 import MarkdownRender from "../Common/Code/MarkdownRender";
-import { ContextActions, DerivedEntryActions, InterfaceActions, LogsActions, ProjectsActions, TileProps } from "@/types/evals/grid";
+import { DerivedEntryActions, InterfaceActions, LogsActions, ProjectsActions, TileProps } from "@/types/evals/grid";
 import { useEffect, useState } from "react";
 import { examples } from "@/constants/logs";
 import { useQueryState } from "nuqs";
 import BaseDropdown from "../Common/Dropdowns/Base";
-import { DropdownMenuItem } from "../UI/dropdown-menu";
+import { DropdownMenuGroup, DropdownMenuItem, DropdownMenuPortal, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger } from "../UI/dropdown-menu";
 import Link from "next/link";
 import { DialogContent } from "../UI/dialog";
 import { Dialog } from "../UI/dialog";
 import { getLogsParameters } from "@/types/evals/logs";
+import { Badge } from "../UI/badge";
 
 const DefaultProject = ({
     projects,
@@ -201,11 +202,72 @@ const DefaultProject = ({
     });
 
     return <>
-        <div className="flex flex-col gap-4 justify-center items-center">
+        <div className="h-[94vh] flex flex-col gap-4 items-center">
             <div className="mt-4 flex justify-center font-semibold">
                 Please select a project, create a project or select an example below
             </div>
-            <div className="flex gap-4">
+            <div className="my-auto flex gap-8">
+                {exampleGif != undefined && <div className="mb-1 flex flex-col items-center gap-4">
+                    <div className="flex gap-4 w-full">
+                        <div className="w-fit">
+                            <BaseDropdown
+                                button={<ActionButton
+                                    tooltip={"Select Example"}
+                                    text={"Select Example"}
+                                    variant={"outline"}
+                                    size="default"
+                                />}
+                            >
+                                <div className="max-h-[80vh] overflow-y-auto">
+                                    {Object.keys(reorganizedExamples).map((group) => (
+                                        <DropdownMenuGroup key={group} className="w-48">
+                                            <DropdownMenuSub>
+                                                <DropdownMenuSubTrigger className="hover:text-white data-[state=open]:text-white">
+                                                    {group}
+                                                </DropdownMenuSubTrigger>
+                                                <DropdownMenuPortal>
+                                                    <DropdownMenuSubContent>
+                                                        {Object.keys(reorganizedExamples[group]).map(ex =>
+                                                            <DropdownMenuItem
+                                                                key={ex}
+                                                                onClick={() => setExample(`${group}/${ex}`)}
+                                                                className="w-48 justify-between"
+                                                            >
+                                                                <span>{ex}</span>
+                                                                {`${group}/${ex}` == example && <Check className="w-4 h-4" />}
+                                                            </DropdownMenuItem>
+                                                        )}
+                                                    </DropdownMenuSubContent>
+                                                </DropdownMenuPortal>
+                                            </DropdownMenuSub>
+                                        </DropdownMenuGroup>
+                                    ))}
+                                </div>
+                            </BaseDropdown>
+                        </div>
+                        {example && <div className="flex my-auto">
+                            <Badge variant="primary">{example.replace("/", " / ")}</Badge>
+                        </div>}
+                    </div>
+                    <div className="text-sm font-semibold w-[600px]">
+                        {exampleDescription}
+                    </div>
+                    <div className="flex flex-col gap-1 border p-1 rounded-lg">
+                        <Image
+                            key={exampleGif}
+                            src={`https://raw.githubusercontent.com/unifyai/unifyai.github.io/main/img/externally_linked/docs/${exampleGif}.gif`}
+                            alt="GIF"
+                            width={600}
+                            height={600}
+                            unoptimized
+                            onClick={() => setImageDialog(true)}
+                            className="cursor-zoom-in rounded-lg"
+                        />
+                        <div className="text-sm font-semibold text-center">
+                            Click image to maximize
+                        </div>
+                    </div>
+                </div>}
                 <div className="relative max-w-[700px] max-h-[700px] overflow-y-auto rounded-md border border-1 p-2">
                     <div className="absolute z-10 top-3 right-12 flex gap-1">
                         <Link href={`https://docs.unify.ai/${exampleLink}`} target="_blank">
@@ -227,43 +289,6 @@ const DefaultProject = ({
                     </div>
                     <MarkdownRender content={`\`\`\`python${exampleCode}\`\`\``} noBackground />
                 </div>
-                {exampleGif != undefined && <div className="mb-1 flex flex-col items-center gap-4">
-                    <div className="w-fit">
-                        <BaseDropdown
-                            button={<ActionButton
-                                tooltip={"Select Example"}
-                                text={example ? example.split("/")[1] : "Select Example"}
-                                variant={"outline"}
-                                size="default"
-                            />}
-                        >
-                            <div className="max-h-[80vh] overflow-y-auto">
-                                {Object.keys(reorganizedExamples).map((group) => {
-                                    return <div key={group} className="w-[300px] mt-2 pb-1 px-3 border-b">
-                                        <div className="font-semibold text-sm mb-1">{group}</div>
-                                        {Object.keys(reorganizedExamples[group]).map(ex =>
-                                            <DropdownMenuItem key={ex} onClick={() => setExample(`${group}/${ex}`)}>
-                                                {ex}
-                                            </DropdownMenuItem>
-                                        )}
-                                    </div>
-                                })}
-                            </div>
-                        </BaseDropdown>
-                    </div>
-                    <div className="text-sm font-semibold w-[600px]">
-                        {exampleDescription}
-                    </div>
-                    <Image
-                        key={exampleGif}
-                        src={`https://raw.githubusercontent.com/unifyai/unifyai.github.io/main/img/externally_linked/docs/${exampleGif}.gif`}
-                        alt="GIF"
-                        width={600}
-                        height={600}
-                        unoptimized
-                        onClick={() => setImageDialog(true)}
-                    />
-                </div>}
             </div>
         </div>
         <Dialog open={imageDialog} onOpenChange={setImageDialog}>
@@ -275,6 +300,8 @@ const DefaultProject = ({
                     width={1400}
                     height={800}
                     unoptimized
+                    className="cursor-zoom-out"
+                    onClick={() => setImageDialog(false)}
                 />
             </DialogContent>
         </Dialog>
