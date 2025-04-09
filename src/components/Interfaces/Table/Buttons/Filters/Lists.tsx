cdd436cfc815d40ec3dc242ef72b879098665580
Haris Mahmood <hmahmood24@users.noreply.github.com>
2025-04-09 15:17:09 +0500

    Fix setting the chosen filter option from the dropdowns on the filter UIs

--- src/components/Interfaces/Table/Buttons/Filters/Lists.tsx
+++ src/components/Interfaces/Table/Buttons/Filters/Lists.tsx
@@ -167,6 +167,7 @@ const ListColumnFilter = ({ interactive, column, columnFilters, setColumnFilterQ
                 newFilters.find(f => f.key === filter.key)!.value === "true" 
                     ? newFilters.find(f => f.key === filter.key)!.value = "false"
                     : newFilters.find(f => f.key === filter.key)!.value = "true"
+                setFilters(newFilters)
             }}
         />
     const filterInput = (filter: ListFilter) => {
@@ -181,6 +182,7 @@ const ListColumnFilter = ({ interactive, column, columnFilters, setColumnFilterQ
                     if (["exists", "isNone"].includes(option.name)) {
                         newFilters.find(f => f.key === filter.key)!.value = "true"
                     }
+                    setFilters(newFilters)
                 }}
             >
                 {["exists", "isNone"].includes(option.name) ? toggleInput(filter) : valueInput(filter, option)}
