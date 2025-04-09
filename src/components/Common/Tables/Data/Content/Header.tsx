f3afa75273f85d898b5ab7b0fe6cba1af5031e9f
Haris Mahmood <hmahmood24@users.noreply.github.com>
2025-04-09 15:17:56 +0500

    Fix the column show button disappearing behind the consecutive header cell due to a non-transparent background

--- src/components/Common/Tables/Data/Content/Header.tsx
+++ src/components/Common/Tables/Data/Content/Header.tsx
@@ -360,7 +360,7 @@ const DataTableHeader = ({
   const isSelected = isNotUtilColumn && !isPlaceholderColumn ? isAllColumnSelected(header) : isAllTableSelected();
   const selectionClass = isSelected ? 'bg-primary' : '';
   const pinnedClass = !isSelected && isPinned && isNotUtilColumn && !isPlaceholderColumn ? 'bg-background' : '';
-  const defaultBgClass = !isSelected && !pinnedClass ? 'bg-background' : '';
+  const defaultBgClass = !isSelected && !pinnedClass ? 'bg-transparent' : '';
   const hoverClass = !isSelected && !pinnedClass && !dropdownOpen && (!isPlaceholderColumn || isIndexColumn) ? 'hover:bg-muted' : '';
 
   const maxLabelWidth = Math.max(Number((style.width as string).split("px")[0]) - (actionButtonRef.current?.clientWidth ?? 0), 10)
