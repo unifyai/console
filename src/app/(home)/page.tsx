a9369d19edd37d72808850099ab9d9464bf3d1b9
nassimberrada <112006029+nassimberrada@users.noreply.github.com>
2026-02-16 14:18:35 +0000

    feat: redirect root to assistants page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
     const redirectUser = async () => {
       try {
         const user = await initializeUser();
-        const redirectUrl = '/interfaces';
+        const redirectUrl = '/assistants';
         router.push(redirectUrl);
       } catch (error) {
         console.error('Error initializing user:', error);
